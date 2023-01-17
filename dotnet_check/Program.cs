d33743d741571c39a77ca39143cd5f78bea2ce54
Saplonily <Saplonily@outlook.com>
2023-01-17 16:24:10 +0800

    test04

--- dotnet_check/Program.cs
+++ dotnet_check/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿File.WriteAllText("test_dir/test.txt", $"test~~~~, {Environment.OSVersion}, {DateTime.Now}");
