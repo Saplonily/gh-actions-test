db7871f84e657214272a17eeb78f38042f6721ad
Saplonily <Saplonily@outlook.com>
2023-01-17 16:28:10 +0800

    test08

--- dotnet_check/Program.cs
+++ dotnet_check/Program.cs
@@ -1,1 +1,2 @@
-﻿File.WriteAllText("../test_dir/test.txt", $"test~~~~, {Environment.OSVersion}, {DateTime.Now}");
+﻿Directory.CreateDirectory("../test_dir");
+File.WriteAllText("../test_dir/test.txt", $"test~~~~, {Environment.OSVersion}, {DateTime.Now}");
